0678ea0e9518f8d841f84090a96c148a7b7b7519
FahriX3 <fahrimandriva138@gmail.com>
2026-07-05 20:00:17 +0700

    add favicon and update title

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -5,7 +5,10 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'SI GERDU PANDANG') }} - Login</title>
+        <title>{{ config('app.name', 'Si-Gerdu Pandang') }}</title>
+        
+        <!-- Favicon -->
+        <link rel="icon" type="image/png" href="{{ asset('images/Logo.png') }}">
 
         <!-- Google Fonts: Inter -->
         <link rel="preconnect" href="https://fonts.googleapis.com">
